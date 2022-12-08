e88e3a08e4c545284dd30f5b0284f25e792184a8
Nickkk14 <112568564+Nickkk14@users.noreply.github.com>
2022-12-07 23:16:11 -0300

    Add files via upload

---
+++ front_-master/equipe-Ecommerce-main/assets/js/pagamento.js
@@ -0,0 +1,16 @@
+const btnPagarAgora = document.getElementById("#BTNpayment")
+btnPagarAgora.addEventListener("click", function () {
+  
+  Swal.fire({
+    
+    title: 'Muito Bem',
+    text: 'Sua compra foi finalizada!!!',
+    confirmButtonColor: "#DD6B55",
+    icon: 'success'
+  })
+})
+function myfunction(){
+var Labelpix = document.getElementById(bc2).value;
+Labelpix.addEventListener("click", function () {
+
+})}
